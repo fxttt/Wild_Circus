f48b40262333ddaf4346e164b415d9216b9a9c7c
fxttt <francois-xavier.toutut@orange.fr>
2016-11-24 19:38:43 +0100

    Indentation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-    // hover sur toutes les images
+ 
     $('img').hover(function(event) {
         event.preventDefault();
-        // le $(this) correspond a l'image ciblée par le hover
-        // .hide() ajout le style display: none; sur l'élément
+        
         $(this).hide();
 
         $(this).fadeIn(1000, function() {
